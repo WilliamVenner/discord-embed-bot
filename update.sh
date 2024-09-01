6562185a41d90d7521315245ccae5101e35b09c0
William <william@venner.io>
2024-09-01 20:14:42 +0100

    Fix update script

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ sudo systemctl stop discord_embed_bot.service &&
 docker stop discord_embed_bot &&
 docker rm discord_embed_bot &&
 docker build -t discord_embed_bot:latest . &&
-docker create --name discord_embed_bot --restart unless-stopped discord_embed_bot:latest &&
 sudo cp discord_embed_bot.service /etc/systemd/system/ &&
+sudo systemctl daemon-reload &&
 sudo systemctl start discord_embed_bot.service
