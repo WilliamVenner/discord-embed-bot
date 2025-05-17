ba7e0d7deff0937bbd4681e764bc6dc7fc03b2aa
William <william@venner.io>
2025-05-17 22:01:58 +0100

    Update user agent

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ mod logging;
 mod tiktok;
 mod yt_dlp;
 
-const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36";
+const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36";
 
 pub struct App {
 	pub discord_bots: Vec<DiscordBotDaemon>,
